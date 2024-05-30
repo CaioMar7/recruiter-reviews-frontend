a231eafa2267e70adc750ba24306fd06318b5115
Caio Marques <caiof.marques@yahoo.com.br>
2024-05-30 10:41:07 -0300

    mudanças no layout.

--- src/components/IconInput/index.tsx
+++ src/components/IconInput/index.tsx
@@ -6,9 +6,9 @@ interface InputProps {
 
 export function IconInput({icon: Icon, ...rest} : InputProps){
     return(
-        <div className="flex items-center gap-2 bg-white p-2 border-sky-100 border-2 rounded">
+        <div className="flex items-center gap-2 bg-white p-2 border-sky-100 border-2 rounded focus-within:border-sky-300 transition ease-in duration-500">
             {Icon && <Icon/>}
-            <input className="outline-none h-8 w-full p-2 text-slate-600"{...rest}/>
+            <input className="focus:border-sky-70 outline-none h-8 w-full p-2 text-slate-600"{...rest}/>
         </div>
     )
 }
