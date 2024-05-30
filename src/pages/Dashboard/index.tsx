14678ce18bb517e00851faf798634c26df58f139
Caio Marques <caiof.marques@yahoo.com.br>
2024-05-30 14:47:13 -0300

    mudanças no layout.

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+
+
+export function Dashboard() {
+
+  return (
+    <>
+      <div className="w-screen h-screen border-2 bg-slate-300">
+        
+        <h1> dashboard </h1>
+      </div>
+    </>
+  )
+}
